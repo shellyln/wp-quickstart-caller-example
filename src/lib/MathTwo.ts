2b437649a6505744a41a88cfbda3589a16ef68bb
shellyln <shellyln@users.noreply.github.com>
2017-06-11 14:14:36 +0900

    Add lint config.

--- src/lib/MathTwo.ts
+++ src/lib/MathTwo.ts
@@ -3,8 +3,7 @@ import {MathExt} from "webpack-typescript-lib-quickstart";
 
 export class MathTwo {
     public addTwice(x: number, y: number): number {
-        let mx = new MathExt();
+        const mx = new MathExt();
         return mx.add(x, y) + mx.add(x, y);
     }
 }
-
